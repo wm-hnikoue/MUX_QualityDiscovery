d4a83f4e3b06e93072c0feba75268d017dfe0664
harold.nikoue@warnermedia.com <harold.nikoue@warnermedia.com>
2023-01-11 21:08:14 +0000

    Added a notebook

---
+++ MUX s3 Ingestion and Aggregation.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+import os
+import .ancillary.eventStream_pb2
+
+# COMMAND ----------
+
+from ancillary import eventStream_pb2
+
+# COMMAND ----------
+
+dbutils.fs.ls("./")
+
+# COMMAND ----------
+
+
